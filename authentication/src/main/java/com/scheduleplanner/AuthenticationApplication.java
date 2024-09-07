804823725adb885b3f76a39137d8aecce920cf17
horvathd1 <horvathdavid.miklos@gmail.com>
2024-09-07 23:59:29 +0200

    edited

--- authentication/src/main/java/com/scheduleplanner/AuthenticationApplication.java
+++ authentication/src/main/java/com/scheduleplanner/AuthenticationApplication.java
@@ -3,11 +3,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@SpringBootApplication(scanBasePackages = {"com.sheduleplanner.common", "com.scheduleplanner"})
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+@SpringBootApplication(scanBasePackages = {"com.scheduleplanner"})
 @EnableAspectJAutoProxy
 public class AuthenticationApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		SpringApplication.run(AuthenticationApplication.class, args);
 	}
 }
